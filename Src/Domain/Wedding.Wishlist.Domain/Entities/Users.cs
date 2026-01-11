c502273e46b9a9b86b9e54cfe63e1ec9cc1fe034
Victor Jardim <victor.jardim@gomara.tech>
2026-01-11 14:20:45 -0300

    .

--- Src/Domain/Wedding.Wishlist.Domain/Entities/Users.cs
+++ Src/Domain/Wedding.Wishlist.Domain/Entities/Users.cs
@@ -9,6 +9,7 @@ namespace Wedding.Wishlist.Domain.Entities
         public string Email { get; set; } = string.Empty;
         public string HashPassword { get; set; } = string.Empty;
         public string HashVersion { get; set; } = string.Empty;
+        public int IsAdmin { get; set; } = 0;
         public DateTimeOffset CreatedAt { get; set; } = DateTimeOffset.UtcNow;
 
     }
